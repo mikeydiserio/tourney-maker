fc580dd03cd2fd60649e9bac0b13fb4fcc03b4ad
mikeydiserio <141356805+mikeydiserio@users.noreply.github.com>
2025-08-04 14:21:51 +1000

    final fuix build

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
     dirs: ["src/app", "src/components", "src/lib"],
     ignoreDuringBuilds: true, // Ignore ESLint errors during builds
   },
+  output: "standalone", // Use standalone output for better performance
+  distDir: "dist", // Set the output directory for the build
 };
 
 export default nextConfig;
